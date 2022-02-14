13066287dc12a3375bff9e75892cad26fd8f2381
ac110718 <allen.h.chan@gmail.com>
2022-02-13 21:12:38 -0500

    lesson03

---
+++ Solidity/making_eth_dapps/ch03/SimpleCoin.sol
@@ -0,0 +1,24 @@
+pragma solidity ^ 0.4.0;
+
+contract SimpleCoin {
+    mapping (address => uint256) public coinBalance;
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    
+    constructor(uint256 _initialSupply) public {
+        coinBalance[msg.sender] = _initialSupply;
+    }
+
+    function transfer(address _to, uint256 _amount) public {
+        require(coinBalance[msg.sender] > _amount);
+        require(coinBalance[_to] + _amount >= coinBalance[_to]);
+        coinBalance[msg.sender] -= _amount;
+        coinBalance[_to] += _amount;
+        emit Transfer(msg.sender, _to, _amount);
+    }
+
+}
+
+/* NOTES
+* nodeless deployment is possible on remix under injected web3 (connect metamask)
+* most EVM operations cost only 3 gas but load from storage is 200, storing to storage is 5000+, contract creation is 32,000
+*/
