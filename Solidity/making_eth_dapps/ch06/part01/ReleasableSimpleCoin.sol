1e863002da54a088f32b498553b34d6813163cd4
ac110718 <allen.h.chan@gmail.com>
2022-02-14 11:15:32 -0500

    ch06

---
+++ Solidity/making_eth_dapps/ch06/part01/ReleasableSimpleCoin.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.18;
+import "./SimpleCoin.sol";
+
+contract ReleasableSimpleCoin is SimpleCoin {
+    bool public released = false;
+    modifier isReleased() {
+        if (!released) {
+            revert();
+        }
+        _;
+    }
+
+    //call parent constructor via modifier
+    constructor(uint256 _initialSupply) SimpleCoin(_initialSupply) public {}
+
+    function release() onlyOwner public {
+        released = true;
+    }
+
+    //overridden (no keyword needed) with isReleased modifier
+    function transfer(address _to, uint256 _amount) isReleased public {
+        super.transfer(_to, _amount);
+    }
+
+    //overridden (no keyword needed) with isReleased modifier
+    function transferFrom(address _from, address _to, uint256 _amount) isReleased public returns (bool) {
+        super.transferFrom(_from, _to, _amount);
+    }
+}
