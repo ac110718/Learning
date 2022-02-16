9cb66f5dd2e5588b828e60fe7a227af20e8ab247
ac110718 <allen.h.chan@gmail.com>
2022-02-16 13:10:21 -0500

    hooks

--- React/01/src/Pet.js
+++ React/01/src/Pet.js
@@ -15,10 +15,13 @@ export default Pet;
 
 */
 
+//anything inside { } will be considered javascript
+// no import needed with JSX
+
 const Pet = (props) => {
     return (
         <div>
-            <h1>{props.name}</h1>
+            <h1>{props.name}</h1> 
             <h2>{props.animal}</h2>
             <h2>{props.breed}</h2>
         </div>
