b2fde5db52967afcedec5228803f1960c2b891db
ac110718 <allen.h.chan@gmail.com>
2022-02-16 13:58:13 -0500

    refactor

--- React/01/src/SearchParams.js
+++ React/01/src/SearchParams.js
@@ -1,19 +1,44 @@
 // notice use of className and htmlFor due to conflicts with class and for in js
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Pet from "./Pet";
+import useBreedList from "./useBreedList";
+import Results from "./Results";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
     //return current value of state and function to update
     //when onChange calls updateLocation (on every keystroke), state has been modified and react will re-render
+    const [pets, setPets] = useState([]);
     const [location, updateLocation] = useState("Seattle, WA");
     const [animal, updateAnimal] = useState("");
     const [breed, updateBreed] = useState("");
-    const breeds = [];
+    const [breeds] = useBreedList(animal)];
+
+    // useEffect allows you to say "do a render of this component first so the user can see something then as soon as the render is done, then do something (the something here being an effect.)
+    
+    useEffect(() => {
+        requestPets();
+    }, []); 
+    
+    // [] is where you declare data dependencies and when to run again..
+    // blank would assume anytime ANY hook changes.. but this func calls setPets and would re-run again
+    // in infinite loop. [] says run it just once on creation
+
+    async function requestPets() {
+        const res = await fetch(`http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`);
+        const json = await res.json();
+        setPets(json.pets);
+    }
 
     return (
         <div className="search-params">
-            <form>
+            <form
+                onSubmit={e => {
+                    e.preventDefault();
+                    requestPets();
+                }}
+            >
                 <label htmlFor="location">
                     location
                     <input 
@@ -58,6 +83,7 @@ const SearchParams = () => {
                 </label>
                 <button>Submit</button>
             </form>
+            <Results pets = {pets} />
         </div>
     );
 };
