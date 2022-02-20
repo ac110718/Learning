757d04af3a795e61faf357cb0b7d60feac0b05cc
ac110718 <allen.h.chan@gmail.com>
2022-02-20 11:29:52 -0500

    ch06

--- EPI/ch06/6-1.cpp
+++ EPI/ch06/6-1.cpp
@@ -12,6 +12,22 @@ bool IsPalindromic(const string& s) {
     return true;
 }
 
+string IntToString(int x) {
+    bool is_negative = false;
+    if (x < 0) {
+        is_negative = true;
+    }
+    string s;
+    do { // ensure at least one iteration.. in case x = 0
+        s += '0' + abs(x % 10); // take "char of zero position" and offset by last digit of x to append
+        x /= 10; // take remaining number for next loop.
+    } while (x);
+
+    s += is_negative ? "-" : "";
+    return {rbegin(s), rend(s)}; // reverse the order since appended LSD first
+}
+
 int main() {
     cout << boolalpha << IsPalindromic("asdbdsa") << endl;
+    cout << IntToString(-100291) << endl;
 }
