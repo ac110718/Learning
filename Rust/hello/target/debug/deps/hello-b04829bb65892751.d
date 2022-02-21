11b5e1f95b6774ca84c437e9348068daa645d14e
ac110718 <allen.h.chan@gmail.com>
2022-02-21 17:33:50 -0500

    rust

--- Rust/hello/target/debug/deps/hello-b04829bb65892751.d
+++
@@ -1,5 +0,0 @@
-/Users/allenchan/Github/Learning/Rust/hello/target/debug/deps/hello-b04829bb65892751: src/main.rs
-
-/Users/allenchan/Github/Learning/Rust/hello/target/debug/deps/hello-b04829bb65892751.d: src/main.rs
-
-src/main.rs:
