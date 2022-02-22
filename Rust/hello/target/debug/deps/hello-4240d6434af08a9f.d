a3548bbff95cb14f8fba4be0df1a275b79cadaa7
ac110718 <allen.h.chan@gmail.com>
2022-02-22 08:27:21 -0500

    gcd program

--- Rust/hello/target/debug/deps/hello-4240d6434af08a9f.d
+++
@@ -1,5 +0,0 @@
-/Users/allenchan/Github/Learning/Rust/hello/target/debug/deps/hello-4240d6434af08a9f: src/main.rs
-
-/Users/allenchan/Github/Learning/Rust/hello/target/debug/deps/hello-4240d6434af08a9f.d: src/main.rs
-
-src/main.rs:
--- Rust/hello/target/debug/hello.d
+++
@@ -1,1 +0,0 @@
-/Users/allenchan/Github/Learning/Rust/hello/target/debug/hello: /Users/allenchan/Github/Learning/Rust/hello/src/main.rs
