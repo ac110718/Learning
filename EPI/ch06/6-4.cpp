59dc2a1db9c87427b22fa36819df5ad37d7ca028
ac110718 <allen.h.chan@gmail.com>
2022-02-24 15:51:57 -0500

    ch06

--- EPI/ch06/6-4.cpp
+++ EPI/ch06/6-4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unordered_map>
+#include <vector>
 
 using namespace std;
 
@@ -19,7 +20,47 @@ int RomanToInteger(const string& s) {
     return sum;
 }
 
+// determine if string of ints is a valid IP address
+
+bool IsValidPart(const string& s) {
+    if (size(s) > 3) {
+        return false;
+    }
+    // only "0" is a valid part.. "01.." "00" etc fail
+    if (s.front() == '0' && size(s) > 1) {
+        return false;
+    }
+    int val = stoi(s);
+    return val <= 255 && val >= 0; // valid range
+}
+
+vector<string> GetValidIpAddress (const string& s) {
+    vector<string> result;
+    // use if and for to drive depth of search
+    for (size_t i = 1; i < 4 && i < size(s); i++) {
+        if (const string first = s.substr(0, i); IsValidPart(first)) {
+            for (size_t j = 1; j < 4 && i + 4 < size(s); j++) {
+                if (const string second = s.substr(i, j); IsValidPart(second)) {
+                    for (size_t k = 1; k < 4 && i + j + k < size(s); k++) {
+                        const string third = s.substr(i + j, k);
+                        const string fourth = s.substr(i+j+k);
+                        if (IsValidPart(third) && IsValidPart(fourth)) {
+                            result.emplace_back(first + "." + second + "." + third + "." + fourth);
+                            // only when meeting all four requirements are you put into results
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return result;
+}
+
 int main() {
     cout << RomanToInteger("LVIIII") << endl;
     cout << RomanToInteger("LIX") << endl;
+    auto x = GetValidIpAddress("19216811");
+    for (auto ip : x) {
+        cout << ip << endl;
+    }
 }
