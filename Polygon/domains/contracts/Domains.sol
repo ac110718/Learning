f098911bb2daaecc3108d418582a1b91745966cd
ac110718 <allen.h.chan@gmail.com>
2022-03-02 16:31:27 -0500

    polygon ens

---
+++ Polygon/domains/contracts/Domains.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.10;
+
+import "hardhat/console.sol";
+
+contract Domains {
+  mapping(string => address) public domains;
+
+  constructor() {
+    console.log("Domain Contract");
+  }
+
+  // calldata keyword non-persistent, not modified vs. memory
+  function register(string calldata name) public {
+    domains[name] = msg.sender;
+    console.log("%s has registered a domain!", msg.sender);
+  }
+
+  function getAddress(string calldata name) public view returns (address) {
+    return domains[name];
+  }
+}
