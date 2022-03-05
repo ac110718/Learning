889a155e53724130484eecb407d67f262b9c1d9b
ac110718 <allen.h.chan@gmail.com>
2022-03-05 11:10:06 -0500

    ch07

--- EPI/ch07/7-1.cpp
+++ EPI/ch07/7-1.cpp
@@ -12,6 +12,12 @@ void InsertAfter(const shared_ptr<ListNode<int>>& node, const shared_ptr<ListNod
     node->next = new_node;
 }
 
+void AppendNode(shared_ptr<ListNode<int>> *node, shared_ptr<ListNode<int>> *tail) {
+    (*tail)->next = *node;
+    *tail = *node; // update tail reference
+    *node = (*node)->next;
+}
+
 shared_ptr<ListNode<int>> SearchList(shared_ptr<ListNode<int>> L, int key) {
     while (L && L->data != key) {
         L = L->next;
@@ -35,6 +41,16 @@ shared_ptr<ListNode<int>> ReverseLL(shared_ptr<ListNode<int>> L) {
     return head;
 }
 
+shared_ptr<ListNode<int>> MergeTwoSortedLists(shared_ptr<ListNode<int>> L1, shared_ptr<ListNode<int>> L2) {
+    shared_ptr<ListNode<int>> dummy_head(new ListNode<int>);
+    auto tail = dummy_head;
+    while(L1 && L2) {
+        AppendNode(L1->data <= L2->data ? &L1 : &L2, &tail); // append least of L1 or L2 to tail builder
+    }
+    tail->next = L1 ? L1 : L2; // append remaining
+    return dummy_head->next; // stayed in place whole time, pointing to 1st element
+}
+
 
 
 int main() {
@@ -47,9 +63,26 @@ int main() {
     InsertAfter(c, d);
     cout << SearchList(a, 2)->data << endl;
     auto reversed = ReverseLL(a);
+    auto reverse_head = reversed;
+    auto x = reverse_head;
     while (reversed != NULL) {
         cout << reversed -> data;
         reversed = reversed -> next;
+    }
+    cout << endl;
+    
+    x = ReverseLL(x);
+    auto e = shared_ptr<ListNode<int>>(new ListNode<int>{1});
+    auto f = shared_ptr<ListNode<int>>(new ListNode<int>{2});
+    auto g = shared_ptr<ListNode<int>>(new ListNode<int>{3});
+    auto h = shared_ptr<ListNode<int>>(new ListNode<int>{4});
+    InsertAfter(e, f);
+    InsertAfter(f, g);
+    InsertAfter(g, h);
+    auto combined = MergeTwoSortedLists(e, x);
+    while (combined != NULL) {
+        cout << combined -> data;
+        combined = combined -> next;
     }
     cout << endl;
 }
---
+++ Rust/hello/target/debug/deps/hello-7d4d39b28d7cd2fc.d
@@ -0,0 +1,5 @@
+/Users/allenchan/Github/Learning/Rust/hello/target/debug/deps/hello-7d4d39b28d7cd2fc: src/main.rs
+
+/Users/allenchan/Github/Learning/Rust/hello/target/debug/deps/hello-7d4d39b28d7cd2fc.d: src/main.rs
+
+src/main.rs:
