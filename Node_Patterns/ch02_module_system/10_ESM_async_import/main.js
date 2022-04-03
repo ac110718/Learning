00d1e938a0f4ae5e51df41a1f0e70c59ad350ae3
ac110718 <allen.h.chan@gmail.com>
2022-04-03 19:00:57 -0400

    async imports

---
+++ Node_Patterns/ch02_module_system/10_ESM_async_import/main.js
@@ -0,0 +1,14 @@
+const SUPPORTED_LANGUAGES = ['el', 'en', 'es', 'it', 'pl']
+const selectedLanguage = process.argv[2]
+
+if (!SUPPORTED_LANGUAGES.includes(selectedLanguage)) {
+    console.error('The specified language is not supported')
+    process.exit(1)
+}
+
+const translationModule = `./strings-${selectedLanguage}.js`
+
+// async function will grab the module import via strings variable
+import(translationModule).then((strings) => {
+    console.log(strings.HELLO)
+})
