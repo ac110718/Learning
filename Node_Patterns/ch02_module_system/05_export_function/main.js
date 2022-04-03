4d977c998a2d94694d91c3a7f33a77c79d7ae2fd
ac110718 <allen.h.chan@gmail.com>
2022-04-03 19:28:29 -0400

    ch03

---
+++ Node_Patterns/ch02_module_system/05_export_function/main.js
@@ -0,0 +1,4 @@
+const logger = require('./logger') 
+
+logger('This is an informational message') // explicit primary functionality of module
+logger.verbose('This is a verbose message') // secondary functionality optional
