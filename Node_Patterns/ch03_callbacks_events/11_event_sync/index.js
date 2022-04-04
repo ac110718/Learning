a2e6ab3f38c6f87939e3946ad855d07ee8bb073f
ac110718 <allen.h.chan@gmail.com>
2022-04-03 23:14:46 -0400

    event emitters

---
+++ Node_Patterns/ch03_callbacks_events/11_event_sync/index.js
@@ -0,0 +1,41 @@
+import { EventEmitter } from 'events'
+import { readFileSync } from 'fs'
+
+class FindRegexSync extends EventEmitter {
+    constructor (regex) {
+        super()
+        this.regex = regex
+        this.files = []
+    }
+
+    addFile (file) {
+        this.files.push(file)
+        return this
+    }
+
+    find () {
+        for (const file of this.files) {
+            let content
+            try {
+                content = readFileSync(file, 'utf8')
+            } catch (err) {
+                this.emit('error', err)
+            }
+            this.emit('fileread', file)
+            const match = content.match(this.regex)
+            if (match) {
+                match.forEach(elem => this.emit('found', file, elem))
+            }
+        }
+        return this
+    }
+}
+
+const findRegexSyncInstance = new FindRegexSync(/hello \w+/g)
+findRegexSyncInstance
+    .addFile('fileA.txt')
+    .addFile('fileB.json')
+    .on('found', (file, match) => console.log(`[Before] Matched "${match}"`))
+    .find()
+    // this never gets invoked or registered
+    .on('found', (file, match) => console.log(`[After] Matched "${match}"`))
