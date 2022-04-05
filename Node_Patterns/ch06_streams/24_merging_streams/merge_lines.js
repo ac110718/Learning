8240423f71fbcd0d5225c49f70f9351ccaba6ab2
ac110718 <allen.h.chan@gmail.com>
2022-04-05 15:02:34 -0400

    merge streams

---
+++ Node_Patterns/ch06_streams/24_merging_streams/merge_lines.js
@@ -0,0 +1,22 @@
+import { createReadStream, createWriteStream } from 'fs'
+import split from 'split'
+
+const dest = process.argv[2]
+const sources = process.argv.slice(3)
+
+const destStream = createWriteStream(dest)
+
+let endCount = 0
+for (const source of sources) {
+    const sourceStream = createReadStream(source, { highWaterMark: 16 })
+    sourceStream.on('end', () => {
+        if (++endCount == sources.length) {
+            destStream.end()
+            console.log(`${dest} created`)
+        }
+    })
+    // pipe many different origins to the same destination (created outside the for loop)
+    sourceStream
+        .pipe(split((line) => line + '\n'))
+        .pipe(destStream, { end: false })
+}
