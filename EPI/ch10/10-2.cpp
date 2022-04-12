87527088bfe739ee214cf228679d145fb7891ec1
ac110718 <allen.h.chan@gmail.com>
2022-04-12 08:59:14 -0400

    ch10

--- EPI/ch10/10-2.cpp
+++ EPI/ch10/10-2.cpp
@@ -37,9 +37,38 @@ vector<Star> FindClosestKStars(vector<Star>::const_iterator stars_begin, const v
 
 auto star_vec = vector<Star>{{1, 2, 3}, {3, 5, 6}, {0, 0, 0}, {10, 11, 12}, {0, 0, 1}, {2, 1, 1}};
 
+// streaming median. Use two heaps.. one to store the bottom half and one to store the top half and look at intersection of the two heaps
+
+vector<double> OnlineMedian(vector<int>::const_iterator sequence_begin, const vector<int>::const_iterator& sequence_end) {
+    priority_queue<int, vector<int>, greater<>> min_heap; // top half
+    priority_queue<int, vector<int>, less<>> max_heap; // bottom half
+    vector<double> result;
+
+    while (sequence_begin != sequence_end) {
+        min_heap.emplace(*sequence_begin++);
+        max_heap.emplace(min_heap.top());
+        min_heap.pop();
+        // default to ensure min_heap has at least 1 or 0 more than max_heap
+        if (size(max_heap) > size(min_heap)) {
+            min_heap.emplace(max_heap.top());
+            max_heap.pop();
+        }
+        result.emplace_back(
+            size(min_heap) == size(max_heap) ? 0.5 * (min_heap.top() + max_heap.top()) : min_heap.top()
+        );
+    }
+    return result;
+}
+
 int main() {
     auto two_closest = FindClosestKStars(star_vec.begin(), star_vec.end(), 3);
     for (auto star : two_closest) {
         cout << star.x << ", " << star.y << ", " << star.z << endl;
     }
+    auto streaming_vec = vector<int>{1, 0, 3, 5, 2, 0, 1};
+    auto streaming_median = OnlineMedian(streaming_vec.begin(), streaming_vec.end());
+    for (auto med : streaming_median) {
+        cout << med << ", ";
+    }
+    cout << endl;
 }
