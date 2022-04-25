fade5397ba41b302f3dbaa94614ef3f8bb07e65f
ac110718 <allen.h.chan@gmail.com>
2022-04-25 06:31:06 -0400

    ch11

--- EPI/ch11/11-2.cpp
+++ EPI/ch11/11-2.cpp
@@ -110,6 +110,57 @@ int FindKthLargest(int k, vector<int>* A_ptr) {
     return FindKth(k, greater<int>(), A_ptr);
 }
 
+
+vector<int> k_search = {3, 1, -1, 2};
+
+// vector of 0... n, but one element is missing and another duplicated
+// XOR of two identical elements will cancel each other out
+// if you take XOR of the given vector and "true" vector of 0-n.. 
+// what will be left is XOR of missing and dup elements
+
+// to further narrow down the search...
+// examine elements 0-n that have same least significant bit as the XOR of missing/dup
+// XOR these elements with elements of A[i] that have these same LSB bits
+// resulting XOR is either an element that shows up exactly one time (the missing element), or three times (the dup element)
+
+// examine A[i] for existence of latest XOR result
+// if found, then dup. If missing, then this element is the missing one
+// the complement is the original XOR_result ^ with the latest element
+
+struct DuplicateAndMissing {
+    int duplicate, missing;
+};
+
+DuplicateAndMissing FindDuplicateMissing(const vector<int>& A) {
+    int miss_XOR_dup = 0;
+    // cancel out all elements showing up exactly 2x from overlap of 0..n and A[i]
+    for (int i = 0; i < size(A); i++) {
+        miss_XOR_dup ^= i ^ A[i];
+    }
+    cout << "mixed element : " << miss_XOR_dup << endl;
+    // find relevant bits with same LSB
+    int differ_bit = miss_XOR_dup & (~(miss_XOR_dup - 1));
+    // store the element that shows up exactly 1x or 3x from overlap of 0..n and A[i] for only candidate elements with LSB
+    int miss_or_dup = 0;
+    for (int i = 0; i < size(A); i++) {
+        if (i & differ_bit) {
+            miss_or_dup ^= i;
+        }
+        if (A[i] & differ_bit) {
+            miss_or_dup ^= A[i];
+        }
+    } // miss_or_dup will be EITHER missing element (1x) or dup element (3x)
+    cout << "missing OR duplicated : " << miss_or_dup << endl;
+    // examine original vector
+    if (find(begin(A), end(A), miss_or_dup) != end(A)) {
+        // must be duplicate. Sift out missing by XOR with original missXORdup mixture element
+        return {miss_or_dup, miss_or_dup ^ miss_XOR_dup};
+    }
+    return {miss_or_dup ^ miss_XOR_dup, miss_or_dup};
+}
+
+vector<int> miss_dup_vec = {5, 3, 0, 3, 1, 2};
+
 int main() {
     MatrixSearch(twoD_array, 3);
     MatrixSearch(twoD_array, 11);
@@ -119,4 +170,6 @@ int main() {
     FindKthLargest(2, &k_search);
     FindKthLargest(3, &k_search);
     FindKthLargest(4, &k_search);
+    auto result2 = FindDuplicateMissing(miss_dup_vec);
+    cout << "duplicate : " << result2.duplicate << " | missing : " << result2.missing << " mixed element : " << to_string(3^4) << endl;
 }
