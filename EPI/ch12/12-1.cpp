80504217bdc7091dc335a320eadc19522e884bd6
ac110718 <allen.h.chan@gmail.com>
2022-04-27 07:17:52 -0400

    ch12

--- EPI/ch12/12-1.cpp
+++ EPI/ch12/12-1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <numeric>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
@@ -31,6 +32,20 @@ vector<vector<string>> FindAnagrams(const vector<string>& dictionary) {
 
 vector<string> input = {"debitcard", "elvis", "silent", "badcredit", "lives", "freedom", "listen", "levis", "money"};
 
+// Can you form a palindrome. Store char into set.. delete on 2nd appearance. Can only have at most 1 leftover entry for middle element
+
+bool CanFormPalindrome(const string& s) {
+    unordered_set<char> chars_with_odd_frequency;
+    for (char c: s) {
+        if (chars_with_odd_frequency.count(c)) {
+            chars_with_odd_frequency.erase(c);
+        } else {
+            chars_with_odd_frequency.insert(c);
+        }
+    }
+    return size(chars_with_odd_frequency) <= 1;
+}
+
 int main() {
     auto result = FindAnagrams(input);
     for (auto vec : result) {
@@ -40,5 +55,9 @@ int main() {
         }
         cout << "}" << endl;
     }
+    string palindrome = "foobaraboof";
+    string not_palindrome = "foobaraboof3aosidjasd";
+    cout << palindrome << boolalpha << " | " << CanFormPalindrome(palindrome) << endl;
+    cout << not_palindrome << boolalpha << " | " << CanFormPalindrome(not_palindrome) << endl;
 }
 
