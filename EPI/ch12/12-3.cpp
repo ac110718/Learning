29d72ce064130bebe59dfd05a96eb1928eec9cfc
ac110718 <allen.h.chan@gmail.com>
2022-05-02 07:01:57 -0400

    ch12

--- EPI/ch12/12-3.cpp
+++ EPI/ch12/12-3.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
 int FindNearestRepitition(const vector<string>& paragraph) {
@@ -18,6 +19,51 @@ int FindNearestRepitition(const vector<string>& paragraph) {
 
 vector<string> para = {"All", "work", "and", "no", "play", "makes", "for", "no", "work", "no", "fun", "and", "no", "results"};
 
+// find smallest subarray containing all values
+// first, find the right index that covers all values within the subset
+// then, slowly shrink left index to try to close of possible repetitions
+
+struct Subarray {
+    int start, end;
+};
+
+Subarray FindSmallestSubarrayCoveringSet(const vector<string> &paragraph, const unordered_set<string> &keywords) {
+    unordered_map<string, int> keywords_to_cover;
+    for (const string &keyword : keywords) {
+        ++keywords_to_cover[keyword];
+    }
+    Subarray result = Subarray{-1, -1};
+    int remaining_to_cover = size(keywords);
+    for (int left = 0, right = 0; right < size(paragraph); ++right) {
+        cout << "left " << left << " | right " << right << " \t" << paragraph[right] << " : " << keywords_to_cover[paragraph[right]] << " | " << remaining_to_cover << " remaining" << endl;
+        if (keywords.count(paragraph[right]) && --keywords_to_cover[paragraph[right]] >=0) {
+            --remaining_to_cover; // keep going but decrement target for total count and specific keyword count
+        }
+        // when you're fully covered.. start moving left to shrink window..
+
+        while(remaining_to_cover == 0) {
+            // update start and end points at beginning or as window shrinks
+            cout << "left " << left << " | right " << right << " \t" << paragraph[left] << " : " << keywords_to_cover[paragraph[left]] << " | " << remaining_to_cover << " remaining" << endl;
+            if ((result.start == -1 && result.end == -1) || right - left < result.end - result.start) {
+                result = {left, right};
+            }
+            if (keywords.count(paragraph[left]) && ++keywords_to_cover[paragraph[left]] > 0) {
+                ++remaining_to_cover; // break out of loop. You no longer cover the set
+            }
+            left++;
+        }
+    }
+    return result;
+}
+
+vector<string> para2 = {"apple", "banana", "apple", "apple", "dog", "cat", "apple", "dog", "banana", "apple", "cat", "dog", "blah"};
+unordered_set<string> keywords = {"banana", "cat"};
+unordered_set<string> keywords2 = {"banana", "dog", "cat"};
+
 int main() {
     cout << FindNearestRepitition(para) << endl;
-}
+    auto result = FindSmallestSubarrayCoveringSet(para2, keywords);
+    cout << result.start << " | " << result.end << endl;
+    auto result2 = FindSmallestSubarrayCoveringSet(para2, keywords2);
+    cout << result2.start << " | " << result2.end << endl;
+}   
