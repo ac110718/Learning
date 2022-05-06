d1de3d5fb10413b861b129f706b79e2988ee0039
ac110718 <allen.h.chan@gmail.com>
2022-05-06 06:43:36 -0400

    react native intermediate

---
+++ React/05_react_native_intermediate/MoodTracker/src/App.tsx
@@ -0,0 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { BottomTabsNavigator } from './screens/BottomTabs.navigator';
+import { AppProvider } from './App.provider';
+import { Platform, UIManager } from 'react-native';
+
+if (Platform.OS === 'android') {
+  if (UIManager.setLayoutAnimationEnabledExperimental) {
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+  }
+}
+
+export const App: React.FC = () => {
+  return (
+    <AppProvider>
+      <NavigationContainer>
+        <BottomTabsNavigator />
+      </NavigationContainer>
+    </AppProvider>
+  );
+};
