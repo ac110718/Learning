eae52671bff33c4d1665cc4c5c78f71d5b9e917a
ac110718 <allen.h.chan@gmail.com>
2022-05-11 06:16:51 -0400

    ch05

--- Rust/ch05_references/lifetime_references/target/debug/lifetime_references.d
+++
@@ -1,1 +0,0 @@
-/Users/allenchan/Github/Learning/Rust/05_references/lifetime_references/target/debug/lifetime_references: /Users/allenchan/Github/Learning/Rust/05_references/lifetime_references/src/main.rs
--- Rust/ch05_references/references/target/debug/references.d
+++
@@ -1,1 +0,0 @@
-/Users/allenchan/Github/Learning/Rust/05_references/references/target/debug/references: /Users/allenchan/Github/Learning/Rust/05_references/references/src/main.rs
