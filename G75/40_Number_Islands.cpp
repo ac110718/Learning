7d31720eb9b5558a5545e36280cf262e976cf2de
ac110718 <allen.h.chan@gmail.com>
2022-08-28 08:49:20 -0400

    40

---
+++ G75/40_Number_Islands.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int result = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == '1') result++;
+                zero_out_island(grid, i, j);
+            }
+        }
+        return result;
+    }
+    void zero_out_island(vector<vector<char>>& grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == '0') return;
+        grid[i][j] = '0';
+        zero_out_island(grid, i-1, j);
+        zero_out_island(grid, i+1, j);
+        zero_out_island(grid, i, j-1);
+        zero_out_island(grid, i, j+1);
+    }
+};
