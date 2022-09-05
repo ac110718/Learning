372056e68099d93ef8b4439bb882386cae9ddf9c
ac110718 <allen.h.chan@gmail.com>
2022-09-05 10:34:20 -0400

    48

---
+++ G75/48_Accounts_Merge.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    unordered_set<string> visited;
+    unordered_map<string, vector<string>> adjacent;
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        for (vector<string>& account : accounts) {
+            string first_email = account[1];
+            for (int i = 2; i < account.size(); i++) {
+                string email = account[i];
+                adjacent[first_email].push_back(email);
+                adjacent[email].push_back(first_email);
+            }
+        }
+        vector<vector<string>> result;
+        for (vector<string>& account : accounts) {
+            string name = account[0];
+            string first_email = account[1];
+            if (visited.find(first_email) == visited.end()) {
+                vector<string> merged;
+                merged.push_back(name);
+                DFS(merged, first_email);
+                sort(merged.begin() + 1, merged.end());
+                result.push_back(merged);
+            }
+        }
+        return result;
+        
+    }
+    
+    void DFS(vector<string>& mergedAccount, string& email) {
+        visited.insert(email);
+        mergedAccount.push_back(email);
+        for (string& neighbor : adjacent[email]) {
+            if (visited.find(neighbor) == visited.end()) {
+                DFS(mergedAccount, neighbor);
+            }
+        }
+    }
+};
