c45ff691f8e91d0730c6cb219b353a309e8f9037
ac110718 <allen.h.chan@gmail.com>
2022-10-06 07:25:33 -0400

    75

--- G75/75_Largest_Rectangle_Histogram.cpp
+++ G75/75_Largest_Rectangle_Histogram.cpp
@@ -5,6 +5,9 @@ public:
         int n = heights.size(), area = 0;
         stack<int> indexes;
         for (int i = 0; i < n; i++) {
+            // keep pushing, but stop when reaching declining height
+            // check the max area of each rectangle you could have built leading into this
+            // with i - 1 as the last stop position
             while (!indexes.empty() && heights[indexes.top()] > heights[i]) {
                 int h = heights[indexes.top()]; indexes.pop();
                 int l = indexes.empty() ? -1 : indexes.top();
