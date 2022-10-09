bebbf1c55dc5a60367a14e5b5d4a8ffcd3547f9f
ac110718 <allen.h.chan@gmail.com>
2022-10-09 06:19:57 -0400

    02

--- G75/02_Valid_Parens.cpp
+++ G75/02_Valid_Parens.cpp
@@ -1,3 +1,10 @@
+// iterate through string, pushing open brackets along the way
+// if not an open bracket, it MUST satisfy closing appropriately. 
+// if so, pop and continue
+// if conditions not satisfied, immediately return false
+// last and final check, you should make it all the way through to end
+// with no exceptions, hence parens.empty() check on the stack.
+
 class Solution {
 public:
     bool isValid(string s) {
