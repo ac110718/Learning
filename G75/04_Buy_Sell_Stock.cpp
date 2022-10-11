0c00bc40a40e20c7a7da05d635e6a9cd4a0e381f
ac110718 <allen.h.chan@gmail.com>
2022-10-11 07:21:34 -0400

    04

--- G75/04_Buy_Sell_Stock.cpp
+++ G75/04_Buy_Sell_Stock.cpp
@@ -1,3 +1,10 @@
+// track and obtain the minimum price seen thus far.. 
+// continuously update the min
+// candidate profit will be curr less min.
+// ... is this bigger than max seen thus far?
+// return max after looping through candidate prices
+
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
