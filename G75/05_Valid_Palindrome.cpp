3bb8385cc459d94f928b86e28b64923bd02cf60e
ac110718 <allen.h.chan@gmail.com>
2022-10-12 07:28:02 -0400

    05

--- G75/05_Valid_Palindrome.cpp
+++ G75/05_Valid_Palindrome.cpp
@@ -1,3 +1,11 @@
+// two pointers at front and back
+// while front pointer is still "in front"...
+// skip pointer to next alphanum on both ends
+// if lowercase alnum == lowercase alnum at both ends.. move to middle
+// else immediate exit
+// if you make it to middle (i.e. front is no longer "in front"), then return true
+
+
 class Solution {
 public:
     bool isPalindrome(string s) {
