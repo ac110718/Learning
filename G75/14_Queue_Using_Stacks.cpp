38f849d0cffc0353151966e78cd5310704adf9ba
ac110718 <allen.h.chan@gmail.com>
2022-10-22 06:32:55 -0400

    14

--- G75/14_Queue_Using_Stacks.cpp
+++ G75/14_Queue_Using_Stacks.cpp
@@ -1,3 +1,13 @@
+// implement Queue with 2 stacks
+// stack #2 will hold elements in last-in order (i.e. oldest element on top)
+// upon a peek.. return the first element (oldest) from s2
+// but if s2 has NOT been loaded yet.. load in from s1.. 
+// freshest item from s1 will be at bottom of s2
+// on push.. push to s1, which will have NEWEST on top
+// on pop.. return item from peek and pop s2
+// if empty.. BOTH stacks need to be empty
+
+
 class MyQueue {
     
 private:
