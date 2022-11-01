4c9acd6938be31582f84188b3e8822d25a318fc7
ac110718 <allen.h.chan@gmail.com>
2022-11-01 09:41:52 -0400

    21

--- G75/21_Add_Binary.cpp
+++ G75/21_Add_Binary.cpp
@@ -7,22 +7,22 @@ public:
         string result = "";
         while ( m >= 0 || n >= 0 ) {
             if (m >= 0) {
-                sum += a[m] - '0';
+                sum += a[m] - '0'; // get the int diff between '1' and '0'
                 m--;
             }
             if (n >= 0) {
                 sum += b[n] - '0';
                 n--;
             }
-            if (sum % 2) {
+            if (sum % 2) { // there is odd number i.e. 1, so add 1 to the result builder
                 result = "1"+ result;
             } else {
-                result = "0" + result;
+                result = "0" + result; // there are two 1s so add a placeholder 0 to the result builder
             }
-            sum /= 2;
+            sum /= 2; // iterate to next position by mod 2 to sum. If it's 1, it will go to zero.. if 2, then there's a 1 in the carryover
         }
         if (sum % 2 == 1) {
-            result = "1" + result;
+            result = "1" + result; // add the final 1 to the result builder
         }
         return result;
         
