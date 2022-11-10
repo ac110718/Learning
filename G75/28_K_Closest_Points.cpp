974fd5913a020a52c6291128e651bbdfa94baffa
ac110718 <allen.h.chan@gmail.com>
2022-11-10 07:03:20 -0500

    28

--- G75/28_K_Closest_Points.cpp
+++ G75/28_K_Closest_Points.cpp
@@ -1,3 +1,9 @@
+// to store the three SMALLEST distances
+// store into MAX heap of k size 
+// so that you compare to the top 3 largest easily
+// automatically remove 4th largest element if size exceeds k
+// load elements out of heap and into vector<int> result
+
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
