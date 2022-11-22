50d17f96e1ebb668b8775750548cb5f3a5a45003
ac110718 <allen.h.chan@gmail.com>
2022-11-22 07:06:38 -0500

    38

--- G75/38_Min_Stack.cpp
+++ G75/38_Min_Stack.cpp
@@ -1,3 +1,8 @@
+// create stack with a global min tracker for any given element within stack
+// have parallel stacks.. one with elements (s1)
+// one with global min up to ith position (s2), with repeated mins as appropriate
+// once min is popped off, should have new global min up to that point.
+
 class MinStack {
     stack<int> s1;
     stack<int> s2;
