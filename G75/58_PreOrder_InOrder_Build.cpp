29905ce8685ab4907b31ff4fb1e97d73dc513204
ac110718 <allen.h.chan@gmail.com>
2022-12-13 06:42:51 -0500

    58

--- G75/58_PreOrder_InOrder_Build.cpp
+++ G75/58_PreOrder_InOrder_Build.cpp
@@ -11,7 +11,7 @@
  */
 class Solution {
     int preIdx;
-    unordered_map<int, int> ordIdx;
+    unordered_map<int, int> ordIdx; // store positioning of specific value
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         preIdx = 0;
@@ -22,11 +22,11 @@ public:
     }
     TreeNode* builder(vector<int>& preorder, int left, int right) {
         if (left > right) return nullptr;
-        int val = preorder[preIdx];
-        preIdx++;
+        int val = preorder[preIdx]; // grab the values in order of tree traversal
+        preIdx++; // get the values in the right order (by updating)
         TreeNode* root = new TreeNode(val);
-        root->left = builder(preorder, left, ordIdx[val] - 1);
-        root->right = builder(preorder, ordIdx[val] + 1, right);
+        root->left = builder(preorder, left, ordIdx[val] - 1); // build out left branch, with updated bounds
+        root->right = builder(preorder, ordIdx[val] + 1, right); // build out right branch, with updated bounds
         return root;
     }
 };
